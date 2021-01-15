a443000b875cbb3645b25c092185f04db0170401
Corentin Hatte <contact@corentin-hatte.eu>
2021-01-15 14:36:17 +0100

    Use crypto for randomID if available

--- tools/randomID.js
+++ tools/randomID.js
@@ -3,5 +3,8 @@
  * @return {string} L'identifiant généré.
  */
 export function randomID() {
+	if (window.crypto && window.crypto.randomBytes) {
+		return window.crypto.randomBytes(20).toString('hex');
+	}
 	return Math.random().toString(36).substr(2, 9);
 }
